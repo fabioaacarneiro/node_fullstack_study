f5d51239b1ca3f348bebe523f064a1d2a994ed7c
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-06-10 14:52:10 -0300

    adicionado exercício 10 do módulo 6

---
+++ 6_EXPRESS/10_404/index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+import users from './users/index.js'
+
+const app = express()
+const port = 3000
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+const __filename = fileURLToPath(import.meta.url)
+const basePath = path.join(path.dirname(__filename), 'templates')
+
+app.use('/users', users)
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(`${basePath}/404.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta: http://localhost:${port}`)
+})
