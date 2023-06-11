dd0e8ecd22929533a770d96972f6382b2320ec8b
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-06-11 12:33:50 -0300

    adicionado exercício 1 do módulo 7

---
+++ 7_TEMPLATE_ENGINE/1_instalacao_handlebars/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import exphbs from 'express-handlebars'
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    res.render('home', {layout: false})
+})
+
+app.listen(3000, () =>{
+    console.log('App rodando em: http://localhost:3000');
+})
