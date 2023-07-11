6273f995e6841bb2bfe87ebb71785554f247f0fa
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-07-10 22:13:22 -0300

    adicionado exercício 7 do módulo 7

---
+++ 7_TEMPLATE_ENGINE/7_with/index.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import exphbs from 'express-handlebars'
+
+const app = express()
+const port = 3030
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard', (req, res) => {
+    const items = ['Item a', 'Item b', 'Item c', 'Item d']
+    res.render('dashboard', {items})
+})
+
+app.get('/post', (req, res) => {
+    const post = {
+        title: 'Aprender Node.js',
+        category: 'Javascript',
+        body: 'Este  artigo vai de ajudar a aprender Node.js....',
+        comments: 4
+    }
+    res.render('blogpost', {post})
+})
+
+app.get('/', (req, res) => {
+
+    const user = {
+        name: 'Nome',
+        surname: 'Sobrenome',
+        age: 99,
+    }
+
+    const mensagemTeste = 'Mensagem de teste'
+
+    const auth = true
+    const aproved = true
+    
+    res.render('home', {user: user, mensagemTeste, auth, aproved})
+})
+
+app.listen(port, () => {
+    console.log(`App rodando em: http://localhost:${port}`);
+})
